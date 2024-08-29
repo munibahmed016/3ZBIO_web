e2e3d9705e71814b1e59a30e632a9f93e903894f
Munib Ahmed <munibahmed016@gmail.com>
2024-08-29 12:41:41 +0500

    Testing

--- src/Components/Categories/FoodSupplements.jsx
+++ src/Components/Categories/FoodSupplements.jsx
@@ -3,10 +3,10 @@ import Footer from '../Footer';
 import Coming from "../../assets/coming-soon2.jpg"
 
 
-const ComingSoon = () => {
+const FoodSupplements = () => {
   return (
     <div className="flex flex-col min-h-screen justify-between items-center text-center">
-      <div className="flex-1 flex flex-col justify-center items-center">
+      <div className="flex-1 flex flex-col justify-center mt-6 items-center">
         <img src={Coming} alt="Coming Soon" className="w-72 md:w-[65%] h-auto" />
       </div>
       <Footer />
@@ -14,4 +14,4 @@ const ComingSoon = () => {
   );
 };
 
-export default ComingSoon;
+export default FoodSupplements;
